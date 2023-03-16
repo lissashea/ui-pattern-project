e0c5d871888033ee2c9bc69eccea032cac89a79a
“Lissa <lwarshaw674@gmail.com>
2023-03-16 07:43:36 -0400

    images

--- ui-slider/main.js
+++ ui-slider/main.js
@@ -1,37 +1,47 @@
+// const slider = document.getElementById('slider');
+// // const prevButton = document.getElementById('prev');
+// // const nextButton = document.getElementById('next');
+
+// let currentIndex = 0;
+
+// // Add your images here
+// const images = 
+//   "https://imgur.com/daJaWEd",
+// //   "https://imgur.com/dxeIlF3",
+// //   "https://imgur.com/0eNdWiN",
+// // ];
+
+// function updateSlider() {
+//   slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
+
+// function prevImage() {
+//   if (currentIndex === 0) {
+//     currentIndex = images.length - 1;
+//   } else {
+//     currentIndex--;
+//   }
+//   updateSlider();
+// }
+
+// function nextImage() {
+//   if (currentIndex === images.length - 1) {
+//     currentIndex = 0;
+//   } else {
+//     currentIndex++;
+//   }
+//   updateSlider();
+// }
+
+// prevButton.addEventListener('click', prevImage);
+// nextButton.addEventListener('click', nextImage);
 const slider = document.getElementById('slider');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
 
-let currentIndex = 0;
-
-// Add your images here
-const images = [
-  "/Users/lissawarshaw/Desktop/repos/projects/ui-pattern-project/ui-slider/images/june3.jpeg",
-  "/Users/lissawarshaw/Desktop/repos/projects/ui-pattern-project/ui-slider/images/june4.jpeg",
-  "/Users/lissawarshaw/Desktop/repos/projects/ui-pattern-project/ui-slider/images/june5.jpeg",
-];
+// Add the image file path here
+const image = "https://imgur.com/daJaWEd";
 
 function updateSlider() {
-  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  slider.innerHTML = `<img src="${image}">`;
 }
 
-function prevImage() {
-  if (currentIndex === 0) {
-    currentIndex = images.length - 1;
-  } else {
-    currentIndex--;
-  }
-  updateSlider();
-}
-
-function nextImage() {
-  if (currentIndex === images.length - 1) {
-    currentIndex = 0;
-  } else {
-    currentIndex++;
-  }
-  updateSlider();
-}
-
-prevButton.addEventListener('click', prevImage);
-nextButton.addEventListener('click', nextImage);
+updateSlider();
