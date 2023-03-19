197c5f6bb692ff98f4c3c5d86d0f9f3a0680bf3a
“Lissa <lwarshaw674@gmail.com>
2023-03-19 17:27:56 -0400

    update image sizing

--- ui-hamburger/main.js
+++ ui-hamburger/main.js
@@ -1,39 +1,50 @@
-		
-		const items = document.querySelectorAll('.item');
-		const modalOverlay = document.querySelector('.modal-overlay');
-		const modal = document.querySelector('.modal');
-		const modalClose = document.querySelector('.modal-close');
+const items = document.querySelectorAll('.item');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal-close');
 
-		items.forEach(item => {
-			item.addEventListener('click', () => {
-				modalOverlay.classList.add('active');
-				modal.classList.add('active');
-			});
-		});
+const breedSelect = document.querySelector('#breed-select');
+const breed = breedSelect.value
+console.log(breed)
 
-		modalOverlay.addEventListener('click', () => {
-			modalOverlay.classList.remove('active');
-			modal.classList.remove('active');
-		});
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    modalOverlay.classList.add('active');
+    modal.classList.add('active');
+    const h2 = modal.querySelector('#breed-name');
+    const breed = breedSelect.value; // get the selected breed from the dropdown
+    h2.innerHTML = breed;
+  });
+});
 
-		modalClose.addEventListener('click', () => {
-			modalOverlay.classList.remove('active');
-			modal.classList.remove('active');
-		});
+modalOverlay.addEventListener('click', () => {
+  modalOverlay.classList.remove('active');
+  modal.classList.remove('active');
+});
 
-	// select all the img elements in the HTML
+modalClose.addEventListener('click', () => {
+  modalOverlay.classList.remove('active');
+  modal.classList.remove('active');
+});
+
+// select all the img elements in the HTML
 const images = document.querySelectorAll(".item img");
 
 // loop through each img element and update its src attribute with a new image URL
-images.forEach((img) => {
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      img.src = data.message; // update the src attribute with the new image URL
-    })
-    .catch((error) => {
-      console.log(error); // handle any errors
-    });
+breedSelect.addEventListener('change', () => {
+  const breed = breedSelect.value;
+  images.forEach((img) => {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        img.src = data.message;
+        const h2 = img.parentElement.querySelector("h2");
+        h2.innerHTML = breed;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }); 
 });
