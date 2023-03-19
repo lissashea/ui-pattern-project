e1d458e23aaf27fb227e107026b5241d7fdbb118
“Lissa <lwarshaw674@gmail.com>
2023-03-19 16:31:11 -0400

    integrating api

--- ui-hamburger/main.js
+++ ui-hamburger/main.js
@@ -1,4 +1,4 @@
-
+		
 		const items = document.querySelectorAll('.item');
 		const modalOverlay = document.querySelector('.modal-overlay');
 		const modal = document.querySelector('.modal');
@@ -20,3 +20,20 @@
 			modalOverlay.classList.remove('active');
 			modal.classList.remove('active');
 		});
+
+	// select all the img elements in the HTML
+const images = document.querySelectorAll(".item img");
+
+// loop through each img element and update its src attribute with a new image URL
+images.forEach((img) => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      img.src = data.message; // update the src attribute with the new image URL
+    })
+    .catch((error) => {
+      console.log(error); // handle any errors
+    });
+});
