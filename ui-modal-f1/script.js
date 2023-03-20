a392d3865706132f53f50d2ceacf5ba1e7dfa9f0
“Lissa <lwarshaw674@gmail.com>
2023-03-20 17:40:25 -0400

    removed name and replaced drivers

--- ui-modal-f1/script.js
+++ ui-modal-f1/script.js
@@ -1,12 +1,16 @@
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.close-button');
+const boxes = document.querySelectorAll('.box');
 
-const nameElement = document.querySelector('.name');
+// const nameElement = document.querySelector('.name');
 const teamElement = document.querySelector('.team');
 const rankingElement = document.querySelector('.ranking');
 const pointsElement = document.querySelector('.points')
-
-const boxes = document.querySelectorAll('.box');
+const engineElement = document.querySelector('.engine');
+const directorElement = document.querySelector('.director');
+const driverOneElement = document.querySelector(".driver1");
+const driverTwoElement = document.querySelector(".driver2");
+const idElement = document.querySelector(".id")
 
 let logoUrls = [];
 
@@ -36,16 +40,24 @@ fetch('https://api-formula-1.p.rapidapi.com/teams', options)
 	})
 	.catch(err => console.error(err));
 
-for (let i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener('click', function() {
-		const info = JSON.parse(this.dataset.info);
-		nameElement.innerText = info.name;
-		teamElement.innerText = 'Team: ' + info.team;
-		pointsElement.innerText = 'Points: ' + info.points;
-		rankingElement.innerText = 'Ranking: ' + info.ranking;
-		modal.style.visibility = 'visible';
-	});
-}
+	for (let i = 0; i < boxes.length; i++) {
+		boxes[i].addEventListener('click', function() {
+			const info = JSON.parse(this.dataset.info);
+			// nameElement.innerText = info.name;
+			driverOneElement.innerHTML = `Driver 1:` + info.driver1;
+			driverTwoElement.innerHTML = `Driver 2:` + info.driver2;
+			teamElement.innerText = 'Team: ' + info.team;
+			rankingElement.innerText = 'Ranking: ' + info.ranking;
+			engineElement.innerText = 'Engine: ' + info.engine;
+			directorElement.innerText = 'Director: ' + info.director;
+			idElement.innerText = `ID:` + info.id;
+
+			modal.style.visibility = 'visible';
+			const logoUrl = logoUrls[i];
+			const modalContent = document.querySelector('.modal-content');
+		});
+	}
+
 closeButton.addEventListener('click', function() {
 	modal.style.visibility = 'hidden';
 });
