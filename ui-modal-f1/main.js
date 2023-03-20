80fdcaee1d392df9340b2d8bcdbc84c3b96ba31e
“Lissa <lwarshaw674@gmail.com>
2023-03-20 09:01:26 -0400

    apis for team, player, ranking

--- ui-modal-f1/main.js
+++ ui-modal-f1/main.js
@@ -1,19 +1,19 @@
 // Get the modal element
-var modal = document.querySelector('.modal');
+let modal = document.querySelector('.modal');
 // Get the close button element
-var closeButton = document.querySelector('.close-button');
+let closeButton = document.querySelector('.close-button');
 
 // Add event listeners to all the boxes
-var boxes = document.querySelectorAll('.box');
-for (var i = 0; i < boxes.length; i++) {
+let boxes = document.querySelectorAll('.box');
+for (let i = 0; i < boxes.length; i++) {
 	// Add an event listener to each box
 	boxes[i].addEventListener('click', function() {
 		// Get the info from the data attribute
-		var info = JSON.parse(this.dataset.info);
+		let info = JSON.parse(this.dataset.info);
 		// Populate the modal with the info
-		var nameElement = document.querySelector('.name');
-		var ageElement = document.querySelector('.age');
-		var occupationElement = document.querySelector('.occupation');
+		let nameElement = document.querySelector('.name');
+		let ageElement = document.querySelector('.age');
+		let occupationElement = document.querySelector('.occupation');
 		nameElement.innerText = info.name;
 		ageElement.innerText = 'Age: ' + info.age;
 		occupationElement.innerText = 'Occupation: ' + info.occupation;
@@ -34,3 +34,47 @@ window.addEventListener('click', function(event) {
 		modal.style.visibility = 'hidden';
 	}
 });
+
+
+//driver api 
+const driver = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d2ffc6293msh1075b39e5ce7c58p1b3558jsneeff180a0616',
+		'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+	}
+};
+
+fetch('https://api-formula-1.p.rapidapi.com/drivers?search=lewi', driver)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+//team api
+const team = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d2ffc6293msh1075b39e5ce7c58p1b3558jsneeff180a0616',
+		'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+	}
+};
+
+fetch('https://api-formula-1.p.rapidapi.com/drivers?search=lewi', team)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+//team ranking
+const teamRankings = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d2ffc6293msh1075b39e5ce7c58p1b3558jsneeff180a0616',
+		'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+	}
+};
+
+fetch('https://api-formula-1.p.rapidapi.com/rankings/teams?season=2021', teamRankings)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
