97eda95a085780b6b7f7f287a170a92698b4fa70
“Lissa <lwarshaw674@gmail.com>
2023-03-20 15:10:00 -0400

    updated images to finally work!!

--- ui-modal-f1/script.js
+++ ui-modal-f1/script.js
@@ -5,6 +5,9 @@ const nameElement = document.querySelector('.name');
 const teamElement = document.querySelector('.team');
 const rankingElement = document.querySelector('.ranking');
 const pointsElement = document.querySelector('.points')
+const imageLogo = document.querySelector('.image')
+
+
 
 const boxes = document.querySelectorAll('.box');
 for (let i = 0; i < boxes.length; i++) {
@@ -28,19 +31,24 @@ window.addEventListener('click', function(event) {
 	}
 });
 
-const myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", "0d2ffc6293msh1075b39e5ce7c58p1b3558jsneeff180a0616");
-myHeaders.append("x-rapidapi-host", "v1.formula-1.api-sports.io");
 
-const requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d2ffc6293msh1075b39e5ce7c58p1b3558jsneeff180a0616',
+		'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+	}
 };
 
-fetch("https://v1.formula-1.api-sports.io/seasons", requestOptions)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.log('error', error));
+fetch('https://api-formula-1.p.rapidapi.com/teams', options)
+	.then(response => response.json())
+	.then(response => {
+		let teamsResponse = response;
+		console.log(teamsResponse);
+		for (let i = 0; i < teamsResponse.response.length; i++) {
+			let teamLogo = teamsResponse.response[i].logo;
+			console.log(teamLogo)
+		}
+	})
+	.catch(err => console.error(err));
+	
